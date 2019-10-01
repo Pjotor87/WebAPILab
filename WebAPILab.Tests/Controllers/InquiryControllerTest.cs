88d81e94f76b38771cea38727d9817015e1d1247
Peter Andersson <pjotor87@hotmail.com>
2019-10-01 22:05:00 +0200

    Tests added

--- WebAPILab.Tests/Controllers/InquiryControllerTest.cs
+++ WebAPILab.Tests/Controllers/InquiryControllerTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebAPILab.Controllers;
 using WebAPILab.DAL;
@@ -12,34 +13,70 @@ namespace WebAPILab.Tests.Controllers
     public class InquiryControllerTest
     {
         static HttpClient client = new HttpClient();
+        static DatabaseContext databaseContext = new DatabaseContext();
         static DateTime testDate = DateTime.Now;
         static Customer testCustomer = new Customer() { CustomerId = 9999, CustomerEmail = "tests@test.com", CustomerName = "Mr Customer", MobileNo = 1111122222, TransactionIds = "999,888" };
         static Transaction testTransaction1 = new Transaction() { CurrencyCode = "SEK", TransactionDateTime = testDate, Amount = 999.50, Status = Constants.Enum.TransactionStatus.Success, TransactionId = 999 };
         static Transaction testTransaction2 = new Transaction() { CurrencyCode = "GBP", TransactionDateTime = testDate, Amount = 48.20, Status = Constants.Enum.TransactionStatus.Success, TransactionId = 888 };
 
+        static bool testsInitialized = false;
         [TestInitialize]
         public void InitializeTests()
         {
-            DatabaseContext context = new DatabaseContext();
-            new DatabaseInitializer(context);
-            context.Customers.Add(testCustomer);
-            context.SaveChanges();
-            context.Transactions.Add(testTransaction1);
-            context.Transactions.Add(testTransaction2);
-            context.SaveChanges();
+            if (!testsInitialized)
+            {
+                new DatabaseInitializer(databaseContext);
+                testsInitialized = true;
+            }
         }
 
         [TestMethod]
-        public void CanGetCustomerById()
+        public async Task CanGetCustomerByIdAsync()
         {
             // Arrange
+            AddTestdataToDatabase();
             InquiryController controller = new InquiryController();
 
             // Act
             HttpResponseMessage response = controller.GetCustomerResponse(testCustomer.CustomerId);
-            Customer result = JsonHelper.DeserializeJson<Customer>(response.Content.ToString());
+            Customer result = JsonHelper.DeserializeJson<Customer>(await response.Content.ReadAsStringAsync());
 
             // Assert
+            AssertTestCustomer(result);
+        }
+
+        [TestMethod]
+        public async Task CanGetCustomerByEmail()
+        {
+            // Arrange
+            AddTestdataToDatabase();
+            InquiryController controller = new InquiryController();
+
+            // Act
+            HttpResponseMessage response = controller.GetCustomerResponse(testCustomer.CustomerEmail);
+            Customer result = JsonHelper.DeserializeJson<Customer>(await response.Content.ReadAsStringAsync());
+
+            // Assert
+            AssertTestCustomer(result);
+        }
+
+        [TestMethod]
+        public async Task CanGetCustomerByIdAndEmail()
+        {
+            // Arrange
+            AddTestdataToDatabase();
+            InquiryController controller = new InquiryController();
+
+            // Act
+            HttpResponseMessage response = controller.GetCustomerResponse(testCustomer.CustomerId, testCustomer.CustomerEmail);
+            Customer result = JsonHelper.DeserializeJson<Customer>(await response.Content.ReadAsStringAsync());
+
+            // Assert
+            AssertTestCustomer(result);
+        }
+
+        private void AssertTestCustomer(Customer result)
+        {
             Assert.IsNotNull(result);
             Assert.AreEqual(testCustomer.CustomerId, result.CustomerId);
             Assert.AreEqual(testCustomer.CustomerEmail, result.CustomerEmail);
@@ -48,25 +85,28 @@ namespace WebAPILab.Tests.Controllers
             Assert.AreEqual(testCustomer.TransactionIds, result.TransactionIds);
         }
 
-        [TestMethod]
-        public void CanGetCustomerByEmail()
-        {
-        }
-
-        [TestMethod]
-        public void CanGetCustomerByIdAndEmail()
-        {
-        }
-
         [TestCleanup]
         public void TearDown()
         {
-            DatabaseContext context = new DatabaseContext();
-            context.Customers.Remove(testCustomer);
-            context.SaveChanges();
-            context.Transactions.Remove(testTransaction1);
-            context.Transactions.Remove(testTransaction2);
-            context.SaveChanges();
+            RemoveTestdataFromDatabase();
+        }
+
+        private static void AddTestdataToDatabase()
+        {
+            databaseContext.Customers.Add(testCustomer);
+            databaseContext.SaveChanges();
+            databaseContext.Transactions.Add(testTransaction1);
+            databaseContext.Transactions.Add(testTransaction2);
+            databaseContext.SaveChanges();
+        }
+
+        private static void RemoveTestdataFromDatabase()
+        {
+            databaseContext.Customers.Remove(testCustomer);
+            databaseContext.SaveChanges();
+            databaseContext.Transactions.Remove(testTransaction1);
+            databaseContext.Transactions.Remove(testTransaction2);
+            databaseContext.SaveChanges();
         }
     }
 }
